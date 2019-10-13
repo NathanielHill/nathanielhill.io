88c32eebee3f8d096874216d52fb4a3396b98977
Nathaniel Hill <nata@goguna.com>
2019-10-12 19:34:55 -0500

    fix next-offline for Next9/Now2

--- next.config.js
+++ next.config.js
@@ -2,6 +2,28 @@ const path = require('path');
 const withOffline = require('next-offline');
 
 module.exports = withOffline({
+  target: 'serverless',
+  transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
